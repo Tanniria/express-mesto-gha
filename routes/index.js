dd12e885e752492091825baba4441acb24cc43aa
Tatiana Smakova <tensmakova@gmail.com>
2023-06-11 12:17:06 +0500

    hotfix: тесты

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ const { NOT_FOUND_ERROR } = require('../utils/errors');
 
 router.use(userRouter);
 router.use(cardsRouter);
-router.use('/*', (req, res) => {
+router.use('/', (req, res) => {
   res.status(NOT_FOUND_ERROR)
     .send({ message: 'Произошла  ошибка на сервере' });
 });
+
+module.exports = router;
