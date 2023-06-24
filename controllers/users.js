8e247349417eb7845ea2ff208e5bc78277980d2d
Tatiana Smakova <tensmakova@gmail.com>
2023-06-24 14:35:41 +0500

    Пр 14: авторизация, регистрация, ошибки

--- controllers/users.js
+++ controllers/users.js
@@ -36,7 +36,7 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', {
         expiresIn: '7d',
       });
-      res.send({ token });
+      res.send({ _id: token });
     })
     .catch(next);
 };
