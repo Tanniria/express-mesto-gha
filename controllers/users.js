8328fde01c7fc0a65b0ff74a0d65f34af1e9f7dd
Tatiana Smakova <tensmakova@gmail.com>
2023-06-24 15:55:26 +0500

    Пр 14: авторизация, регистрация, ошибки

--- controllers/users.js
+++ controllers/users.js
@@ -15,12 +15,8 @@ module.exports.createUser = (req, res, next) => {
     .then((hash) => User.create({
       name, about, avatar, email, password: hash,
     }))
-    .then((user) => res.status(STATUS_CREATED).send({
-      email: user.email,
-      name: user.name,
-      about: user.about,
-      avatar: user.avatar,
-      _id: user._id,
+    .then(() => res.status(STATUS_CREATED).send({
+      name, about, avatar, email,
     }))
     .catch((err) => {
       if (err.code === 11000) {
